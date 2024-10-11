a5d8a1d0d789fbe70d462da41570b3a6dcd7c4b9
nandosabino <sabinofernando05@gmail.com>
2024-10-11 12:29:45 -0300

    resolvendo erro 404

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import Home from './pages/Home'
 import ListUsers from "./pages/ListUsers";
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         element: <Home />,
